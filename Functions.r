30865152803eaab0ab7c9f1eda2a1e37f24e823e
Buwaneka De Silva <buwanekapasan@gmail.com>
2019-03-01 20:59:09 +0530

    Functions with R

---
+++ Functions.r
@@ -0,0 +1,13 @@
+#Parse in arguments to the function
+sum_a_b <-function(a,b) {
+  result <- a + b #calculation
+  return (result) #return the result
+}
+sum_a_b(10,5)#Arguments for the function
+
+#Parse in arguments to the function
+pow <- function(a,b){
+  result <- a^b#calculation
+  return(result)#return the result
+}
+pow(10,3)#Arguments for the function
