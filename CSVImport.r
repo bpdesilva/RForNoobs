7c7d1428b38f2e4ee9f78a9129177ca6f6c7d603
Buwaneka De Silva <buwanekapasan@gmail.com>
2019-03-31 15:05:10 +0530

    CSV Imports

---
+++ CSVImport.r
@@ -0,0 +1,6 @@
+ bmi = read.csv("//Path//bmi.csv",head=TRUE) #Assign csv to variable
+ bmi #Prints CSV
+ medidan (bmi$bmi) #Print Dataset median
+ mean (bmi$bmi) #Print Dataset mean
+ sortedBMI = sort(bmi$bmi)
+ sortedBMI #Print Sorted
