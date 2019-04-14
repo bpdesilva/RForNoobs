618e98639c8e05ed160ac8116ecaf1c9732c2999
Buwaneka De Silva <buwanekapasan@gmail.com>
2019-04-15 03:03:32 +0530

    GGPLOT First Attempt

--- Source Code/Correlation.r
+++ Source Code/Correlation.r
@@ -7,7 +7,6 @@ meanSD = mean(sal$Salary)
 plot(sal$Salary, dnorm(sal$Salary,meanSD,salSD), type = "1")
 sortedSalary = sort(sal$Salary)
 shapiro.test(sortedSalary)
-sortedSalary = sort(sal$Salary)
 plot(sortedSalary, dnorm(sortedSalary,meanSD,salSD), type = "l")
 corVal = cor(sal$Salary,sal$Yrs.since.phd, method="pearson")
 corVal
