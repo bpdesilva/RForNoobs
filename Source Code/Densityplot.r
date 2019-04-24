9ecce3da40a4cfdc5189db6b87fce4041e26831d
bpdesilva <buwanekapasan@gmail.com>
2019-04-25 01:18:52 +0530

    Multiple Density Plots with Mean

--- Source Code/Densityplot.r
+++ Source Code/Densityplot.r
@@ -8,3 +8,13 @@ gpplot(sal,aes(x=Salary,fill=Position))+
 # Density Random to fit data with Legend & Opaque Overlay fill for density plot
 ggplot(sal, aes(Salary, fill = Position, colour = Position)) +
   geom_density(alpha = 0.1)
+
+install.packages("plyr")
+library(plyr)
+means <- ddply(sal, "Position", summarise, grp.mean=mean(Salary))
+head(means)
+
+ggplot(sal, aes(x=Salary))+
+  geom_density()+facet_grid(Position ~ .)+
+  geom_vline(data=means, aes(xintercept=grp.mean, color="red"),
+             linetype="dashed")
