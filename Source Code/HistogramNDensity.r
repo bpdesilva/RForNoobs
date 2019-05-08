7590255ada6a98c689f035b740e4ee18732b62ae
bpdesilva <buwanekapasan@gmail.com>
2019-05-09 01:18:57 +0530

    Fix : Density & Normal Curves on Monte-Carlo

--- Source Code/HistogramNDensity.r
+++ Source Code/HistogramNDensity.r
@@ -2,22 +2,30 @@ library(ggplot2) # Import library
 sal = read.csv("/Salaries.csv", header = TRUE) # Open CSV
 #Parse in Data from CSV
 
+#Density 
 ggplot(data=sal, aes(sal$Salary)) + #Defines X Axis
   geom_histogram(aes(y=..density..), #Defines Y Axis
                  colour="black", fill="white") + # Bar Colors
   geom_density(alpha=.2, fill="#FF6666") + #Bell Curve Shade
   labs(title="Histogram for Salary", x="Salary", y="Density") + # Label Name
-  geom_vline(aes(xintercept=mean(sal$Salary)), # Mean Dotted Line   
-             color="red", linetype="dashed", size=1) # Dotted Line Formatting
+  geom_vline(aes(xintercept=median(sal$Salary),
+                 color="median"), linetype="dashed",
+             size=1) +
+  geom_vline(aes(xintercept=mean(sal$Salary),
+                 color="mean"), linetype="dashed",
+             size=1) +
+  scale_color_manual(name = "statistics", values = c(median = "blue", mean = "red"))
 
-ggplot(sal, aes(x = Position, fill =Salary)) +
-        geom_density(position="identity", alpha=0.6) +
-        scale_x_continuous(name = "Mean ozone in\nparts per billion",
-                           breaks = seq(0, 200, 25),
-                           limits=c(0, 200)) +
-        scale_y_continuous(name = "Density") +
-        ggtitle("Density plot of mean ozone") +
-        theme_bw() +
-        theme(plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
-              text = element_text(size = 12, family = "Tahoma")) +
-        scale_fill_brewer(palette="Accent")
+#Normal
+salSD = sd(sal$Salary)
+salMean = mean(sal$Salary)
+ggplot(data=sal, aes(sal$Salary)) +
+  stat_function(fun = dnorm, args = c(mean = salMean, sd = salSD), col = "tomato")+
+  labs(title="Histogram for Salary", x="Salary", y="Density") + # Label Name
+  geom_vline(aes(xintercept=median(sal$Salary),
+                 color="median"), linetype="dashed",
+             size=1) +
+  geom_vline(aes(xintercept=mean(sal$Salary),
+                 color="mean"), linetype="dashed",
+             size=1) +
+  scale_color_manual(name = "statistics", values = c(median = "blue", mean = "red"))
--- Source Code/Prediction.r
+++ Source Code/Prediction.r
@@ -1,5 +1,5 @@
 sal  = read.csv("//<Path>//")
 corVal = cor(sal$Salary,sal$Yrs.since.phd, method="pearson")
 Model = lm(Salary ~ Yrs.since.phd, data = sal)
-Testdata = data.frame(Salary=c(12000,10000,100000))
+Testdata = data.frame(Salary=c(12000,100000,400000))
 predict(Model, newdata = Testdata, interval = "confidence")
