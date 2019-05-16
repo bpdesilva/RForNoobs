8132cf390713ce903ef9c7e553c22d2c34ba4ff9
bpdesilva <buwanekapasan@gmail.com>
2019-05-17 03:25:00 +0530

    Few Updates

--- Source Code/MultipleRegression.r
+++ Source Code/MultipleRegression.r
@@ -1,6 +1,7 @@
 sal = read.csv("Salaries.csv", header = TRUE)
+ServiceYears = sal$Yrs.service
 Salary = sal$Salary
 SincePhd = sal$Yrs.since.phd
-Model = lm (Salary ~SincePhd)
-predict(Model,data.frame(SincePhd=2)))
+Model = lm (Salary ~SincePhd+ServiceYears)
+predict(Model,data.frame(SincePhd=2,ServiceYears=3)))
 corval = cor(salary,SincePhd,method="pearson")
