2b8939988471e8899be1d0df77cce342c6645f23
bpdesilva <buwanekapasan@gmail.com>
2019-06-02 16:22:31 +0530

    Fix plot

--- Source Code/NormalCurve.r
+++ Source Code/NormalCurve.r
@@ -1,7 +1,10 @@
 library(ggplot2) # Import library
-sal = read.csv("/Salaries.csv", header = TRUE) # Open CSV
+dataset = read.csv("/Salaries.csv", header = TRUE) # Open CSV
 
-ggplot(data=dataset, aes(dataset$Price)) +
+salaryMean = mean(dataset$Salary)
+salarySD = sd(dataset$Salary)
+
+ggplot(data=dataset, aes(dataset$Salary)) +
   geom_histogram(aes(y=..density..),
                  colour="black", fill="white") +
   stat_function(fun = dnorm, args = c(mean = salaryMean, sd = salarySD), col="#32CD32", geom = 'area', fill = '#00FF00', alpha = 0.2)+
